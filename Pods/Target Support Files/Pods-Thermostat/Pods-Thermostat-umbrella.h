0746a1c4f344c849ce7f7167ae8db49b72dbedd2
EugeneKlyuenkov <dawd>
2022-06-15 09:59:58 +0300

    MAR-390-[renamed project]

--- Pods/Target Support Files/Pods-Thermostat/Pods-Thermostat-umbrella.h
+++ Pods/Target Support Files/Pods-Thermostat/Pods-Thermostat-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_ThermostatControlVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_ThermostatControlVersionString[];
+FOUNDATION_EXPORT double Pods_ThermostatVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_ThermostatVersionString[];
 
